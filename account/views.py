9e6f30be3a03c5b615d6c7391a0e8adbe614a1c2
GiselleGewelle <satavamas@mail.ru>
2023-07-10 20:38:39 +0600

    add

---
+++ account/views.py
@@ -0,0 +1,96 @@
+from django.contrib.auth import get_user_model
+from django.urls.conf import path
+from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import ListModelMixin
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from account import serializers
+from account.send_mail import send_confirmation_email
+from rest_framework import status
+from rest_framework import generics
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from .serializers import ChangePasswordSerializer
+from rest_framework.permissions import IsAuthenticated
+
+
+class ChangePasswordView(generics.UpdateAPIView):
+    """
+    An endpoint for changing password.
+    """
+    serializer_class = ChangePasswordSerializer
+    model = User
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self, queryset=None):
+        obj = self.request.user
+        return obj
+
+    def update(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        serializer = self.get_serializer(data=request.data)
+
+        if serializer.is_valid():
+            # Check old password
+            if not self.object.check_password(serializer.data.get("old_password")):
+                return Response({"old_password": ["Wrong password."]}, status=status.HTTP_400_BAD_REQUEST)
+            # set_password also hashes the password that the user will get
+            self.object.set_password(serializer.data.get("new_password"))
+            self.object.save()
+            response = {
+                'status': 'success',
+                'code': status.HTTP_200_OK,
+                'message': 'Password updated successfully',
+                'data': []
+            }
+
+            return Response(response)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# from account.send_mail import
+
+
+User = get_user_model()
+
+
+class UserViewSet(ListModelMixin, GenericViewSet):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
+    permission_classes = (AllowAny,)
+
+    @action(['POST'], detail=False)
+    def register(self, request, *args, **kwargs):
+        serializer = serializers.RegisterSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        if user:
+            try:
+                send_confirmation_email(user.email, user.activation_code)
+            except Exception as e:
+                return Response({'msg': 'Registered, but troubles with email!',
+                                 'data': 'serializer.data'}, status=201)
+            return Response(serializer.data, status=201)
+
+    @action(['GET'], detail=False, url_path='activate/(?P<uuid>[0-9A-Fa-f-]+)')
+    def activate(self, request, uuid):
+        try:
+            user = User.objects.get(activation_code=uuid)
+        except User.DoesNotExist:
+            return Response({'msg': 'Invalid link or link expired!'}, status=400)
+        user.is_active = True
+        user.activation_code = ''
+        user.save()
+        return Response({'msg': 'Successfully activated!'}, status=200)
+
+
+class LoginView(TokenObtainPairView):
+    permission_classes = (AllowAny,)
+
+
+class RefreshView(TokenRefreshView):
+    permission_classes = (AllowAny,)
